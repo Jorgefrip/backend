6af4000e047edf9c322461d409e1cc392508034a
Jorgefrip <98815646+Jorgefrip@users.noreply.github.com>
2023-08-15 19:02:49 -0500

    variables y metodos

---
+++ variables.php
@@ -0,0 +1,11 @@
+<?php
+$nombre = "Jorge";
+$edad = 24;
+$double = 25.6;
+$boolean = false;
+$nombreCurso = "Backend";
+//CONCATENACION
+$concatenacion = $nombre . $edad .  $double;
+echo $concatenacion;
+?>
+
